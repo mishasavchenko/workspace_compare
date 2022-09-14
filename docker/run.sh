5981ba8598dec80fc4245c11dd513c5e558d7885
Misha Savchenko <savchenkmisha@gmail.com>
2022-09-13 22:05:07 -0500

    Fixed run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 APP_NAME="workspace_compare"
-APP_PATH=$PWD"/../src"
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+APP_PATH=${SCRIPT_DIR}"/../src"
+
+echo $APP_PATH
 docker run -it \
            --rm \
            -p 8010:8010 \
